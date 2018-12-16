8dcd2f66515f360b3d16b22dddd076f078d9a9b7
Steve B <steve.birstok@nbcuni.com>
2018-12-16 15:57:21 -0500

    updating files

---
+++ src/db.php
@@ -0,0 +1,31 @@
+<?php
+
+    // If you installed via composer, just use this code to require autoloader on the top of your projects.
+    require 'vendor/autoload.php';
+
+    // Using Medoo namespace
+    use Medoo\Medoo;
+
+    // Initialize
+    $database = new Medoo([
+        'database_type' => 'mysql',
+        'database_name' => getenv('DATABASE_NAME'),
+        'server' => getenv('DATABASE_ENDPOINT'),
+        'username' => getenv('DATABASE_USERNAME'),
+        'password' => getenv('DATABASE_PASSWORD')
+    ]);
+
+    // Enjoy
+    $database->insert('account', [
+        'user_name' => 'foo',
+        'email' => 'foo@bar.com'
+    ]);
+
+    $data = $database->select('account', [
+        'user_name',
+        'email'
+    ], [
+        'user_id' => 50
+    ]);
+
+    echo json_encode($data);
