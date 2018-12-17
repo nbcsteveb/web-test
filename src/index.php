7040ee1de0f4f8200841c39baa89bb3346cb7625
Steve B <steve.birstok@nbcuni.com>
2018-12-17 11:25:49 -0500

    add the current time

--- src/index.php
+++ src/index.php
@@ -1,5 +1,8 @@
 <?php
     $hostname = gethostname();
     $ip = $_SERVER['REMOTE_ADDR'];
+    $current_time = date('m/d/Y H:i:s e');
+
     echo "Hello, ${ip}. I am ${hostname}.";
+    echo "<br />The time is now: ${current_time}";
 
