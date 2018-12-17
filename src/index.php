26c056a430dbfa131aa3e3638de089d02b69f2c5
Steve B <steve.birstok@nbcuni.com>
2018-12-17 13:46:26 -0500

    add version

--- src/index.php
+++ src/index.php
@@ -1,8 +1,10 @@
 <?php
+    $version = '0.0.1';
     $hostname = gethostname();
     $ip = $_SERVER['REMOTE_ADDR'];
     $current_time = date('m/d/Y H:i:s e');
 
     echo "Hello, ${ip}. I am ${hostname}.";
     echo "<br />The time is now: ${current_time}";
+    echo "<br />Verison: ${version}";
 
